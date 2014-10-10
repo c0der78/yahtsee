c88f04bfbdfa8390d3dfec48f4f54a1209a94b38
Ryan Jennings <c0der78@gmail.com>
2014-10-10 00:53:59 -0700

    Apparently works-for-me is a crappy excuse.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 
 #include <stdlib.h>
 #include <iostream>
-#include <arg3dice/yaht/engine.h>
+#include "yaht/engine.h"
 
 #include "caca.h"
 
@@ -9,7 +9,6 @@
 
 using namespace std;
 
-void display_dice(caca_game &, player *);
 
 int main(int argc, char **argv)
 {
@@ -24,30 +23,7 @@ int main(int argc, char **argv)
 
         if (game.state() == ASK_NAME)
         {
-            int ch = game.read_input();
-
-            if (ch == CACA_KEY_RETURN)
-            {
-                string name = game.get_buffer();
-
-                engine::instance()->add_player(name);
-
-                game.set_state(PLAYING);
-
-                game.clear_buffer();
-
-                game.clear();
-            }
-            else
-            {
-                int len = game.add_to_buffer(ch);
-
-                game.put(80 + len - 1, 22, ch);
-
-                game.set_cursor(80 + len, 22);
-            }
-
-            game.refresh();
+            game.state_ask_name();
 
             continue;
         }
@@ -56,25 +32,13 @@ int main(int argc, char **argv)
 
         char input = game.read_input();
 
-        switch (input)
+        switch (tolower(input))
         {
         case 'r':
-            if (player->roll_count() < 3)
-            {
-                player->roll();
-                game.new_frame();
-                display_dice(game, player);
-                game.refresh();
-            }
-            else
-            {
-                game.put(80, 28, "You must choose a score after three rolls.");
-                game.refresh();
-            }
+            game.action_roll(player);
             break;
         case 'f':
-            player->score().lower_score(scoresheet::FULL_HOUSE, player->calculate_lower_score(scoresheet::FULL_HOUSE));
-            game.refresh(true);
+            game.action_full_house(player);
             break;
         case 'k':
         case 't':
@@ -82,12 +46,10 @@ int main(int argc, char **argv)
             game.add_to_buffer(input);
             break;
         case 'y':
-            player->score().lower_score(scoresheet::YACHT, player->calculate_lower_score(scoresheet::YACHT));
-            game.refresh(true);
+            game.action_yaht(player);
             break;
         case 'c':
-            player->score().lower_score(scoresheet::CHANCE, player->calculate_lower_score(scoresheet::CHANCE));
-            game.refresh(true);
+            game.action_chance(player);
             break;
 
         case 'q':
@@ -101,42 +63,34 @@ int main(int argc, char **argv)
 
                 if (buffer.length() > 0)
                 {
-                    switch (buffer[0])
+                    switch (tolower(buffer[0]))
                     {
                     case 'k':
                     {
                         if (input == '3')
                         {
-                            player->score().lower_score(scoresheet::KIND_THREE, player->calculate_lower_score(scoresheet::KIND_THREE));
-                            game.refresh(true);
+                            game.action_three_of_a_kind(player);
                         }
                         else if (input == '4')
                         {
-                            player->score().lower_score(scoresheet::KIND_FOUR, player->calculate_lower_score(scoresheet::KIND_FOUR));
-                            game.refresh(true);
+                            game.action_four_of_a_kind(player);
                         }
                         break;
                     }
                     case 's':
                     {
-                        int n = input - '0';
-
-                        player->score().upper_score(n, player->calculate_upper_score(n));
-
-                        game.refresh(true);
+                        game.action_score(player, input - '0');
                         break;
                     }
                     case 't':
                     {
                         if (input == '4')
                         {
-                            player->score().lower_score(scoresheet::STRAIGHT_SMALL, player->calculate_lower_score(scoresheet::STRAIGHT_SMALL));
-                            game.refresh(true);
+                            game.action_small_straight(player);
                         }
                         else if (input == '5')
                         {
-                            player->score().lower_score(scoresheet::STRAIGHT_BIG, player->calculate_lower_score(scoresheet::STRAIGHT_BIG));
-                            game.refresh(true);
+                            game.action_large_straight(player);
                         }
 
                     }
@@ -146,13 +100,7 @@ int main(int argc, char **argv)
                 }
                 else
                 {
-                    int d = input - '0' - 1;
-                    if (player->is_kept(d))
-                        player->keep_die(d, false);
-                    else
-                        player->keep_die(d, true);
-                    display_dice(game, player);
-                    game.refresh();
+                    game.action_select_die(player, input - '0' - 1);
                 }
             }
         }
@@ -161,22 +109,3 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void display_dice(caca_game &game, player *player)
-{
-    char buf[BUFSIZ + 1] = {0};
-    snprintf(buf, BUFSIZ, "Roll %d of 3. (Use '#' to keep):", player->roll_count());
-    game.put(80, 20, buf);
-    int x = 82;
-
-    game.put(80, 22, "#  1 │ 2 │ 3 │ 4 │ 5");
-    game.put(80, 23, "  ───┴───┴───┴───┴───");
-    game.put(80, 24, "  ");
-    for (size_t i = 0; i < player->die_count(); i++)
-    {
-        game.put(x++, 24, player->is_kept(i) ? '*' : ' ');
-
-        game.put(x++, 24, to_string(player->d1e(i).value()).c_str());
-
-        x += 2;
-    }
-}
--- src/yaht_game.h
+++ src/yaht_game.h
@@ -1,6 +1,8 @@
 
 #include "caca_game.h"
 
+#include "yaht/engine.h"
+
 using namespace arg3::yaht;
 
 typedef enum
@@ -123,6 +125,196 @@ public:
         }
 
     }
+
+
+    void display_already_scored()
+    {
+        put(80, 28, "You've already scored that.");
+        refresh();
+    }
+
+    void display_dice(player *player)
+    {
+        char buf[BUFSIZ + 1] = {0};
+        snprintf(buf, BUFSIZ, "Roll %d of 3. (Use '#' to keep):", player->roll_count());
+        put(80, 20, buf);
+        int x = 82;
+
+        put(80, 22, "#  1 │ 2 │ 3 │ 4 │ 5");
+        put(80, 23, "  ───┴───┴───┴───┴───");
+        put(80, 24, "  ");
+        for (size_t i = 0; i < player->die_count(); i++)
+        {
+            put(x++, 24, player->is_kept(i) ? '*' : ' ');
+
+            put(x++, 24, to_string(player->d1e(i).value()).c_str());
+
+            x += 2;
+        }
+    }
+
+    void state_ask_name()
+    {
+        int ch = read_input();
+
+        if (ch == CACA_KEY_RETURN)
+        {
+            string name = get_buffer();
+
+            engine::instance()->add_player(name);
+
+            set_state(PLAYING);
+
+            clear_buffer();
+
+            clear();
+        }
+        else
+        {
+            int len = add_to_buffer(ch);
+
+            put(80 + len - 1, 22, ch);
+
+            set_cursor(80 + len, 22);
+        }
+
+        refresh();
+    }
+
+
+    void action_select_die(player *player, int d)
+    {
+        if (player->is_kept(d))
+            player->keep_die(d, false);
+        else
+            player->keep_die(d, true);
+
+        display_dice(player);
+        refresh();
+    }
+
+    void action_roll(player *player)
+    {
+        if (player->roll_count() < 3)
+        {
+            player->roll();
+            new_frame();
+            display_dice(player);
+            refresh();
+        }
+        else
+        {
+            put(80, 28, "You must choose a score after three rolls.");
+            refresh();
+        }
+    }
+
+    void action_full_house(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::FULL_HOUSE))
+        {
+            player->score().lower_score(scoresheet::FULL_HOUSE, player->calculate_lower_score(scoresheet::FULL_HOUSE));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_yaht(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::YACHT))
+        {
+            player->score().lower_score(scoresheet::YACHT, player->calculate_lower_score(scoresheet::YACHT));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_chance(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::CHANCE))
+        {
+            player->score().lower_score(scoresheet::CHANCE, player->calculate_lower_score(scoresheet::CHANCE));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_three_of_a_kind(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::KIND_THREE))
+        {
+            player->score().lower_score(scoresheet::KIND_THREE, player->calculate_lower_score(scoresheet::KIND_THREE));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_four_of_a_kind(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::KIND_FOUR))
+        {
+            player->score().lower_score(scoresheet::KIND_FOUR, player->calculate_lower_score(scoresheet::KIND_FOUR));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_score(player *player, int n)
+    {
+        if (!player->score().upper_score(n))
+        {
+            player->score().upper_score(n, player->calculate_upper_score(n));
+
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_small_straight(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::STRAIGHT_SMALL))
+        {
+            player->score().lower_score(scoresheet::STRAIGHT_SMALL, player->calculate_lower_score(scoresheet::STRAIGHT_SMALL));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+    void action_large_straight(player *player)
+    {
+        if (!player->score().lower_score(scoresheet::STRAIGHT_BIG))
+        {
+            player->score().lower_score(scoresheet::STRAIGHT_BIG, player->calculate_lower_score(scoresheet::STRAIGHT_BIG));
+            refresh(true);
+        }
+        else
+        {
+            display_already_scored();
+        }
+    }
+
+
 private:
 
     game_state state_;
