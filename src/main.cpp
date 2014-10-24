02c56c734bfc897bf9ab85e5da42c1f62d9f249e
Ryan Jennings <c0der78@gmail.com>
2014-10-23 23:13:03 -0700

    connect with a few apis for testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,30 @@
 
 #include "yaht_game.h"
 
+#include <arg3net/http_client.h>
+#include <arg3/util.h>
+
 using namespace std;
 
+using namespace arg3;
 
 int main(int argc, char **argv)
 {
     yaht_game game;
+
+    net::http_client ipinfo("ipinfo.io");
+
+    string response = ipinfo.get("ip").response();
+
+    string host = arg3::trim(response);
+
+    net::http_client api("localhost:1337");
+
+    string payload = "{\"type\": \"yahtsee\", \"host\":\"" + host + "\", \"port\":6335}\n";
+
+    api.add_header("Content-Type", "application/json");
+
+    api.set_payload(payload).post("api/v1/games/register");
 
     game.start();
 
