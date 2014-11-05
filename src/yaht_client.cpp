fe5f754a533cd34c8baa5a771bfa7a48e871915c
Ryan Jennings <c0der78@gmail.com>
2014-11-05 09:06:53 -0800

    Add missing files

---
+++ src/yaht_client.cpp
@@ -0,0 +1,75 @@
+#include "yaht_client.h"
+#include <arg3dice/yaht/engine.h>
+#include <arg3json/object.h>
+#include "yaht_game.h"
+
+using namespace arg3;
+using namespace arg3::net;
+
+yaht_client::yaht_client(yaht_game *game, SOCKET sock, const sockaddr_in &addr) : buffered_socket(sock, addr), game_(game)
+{}
+
+yaht_client::yaht_client(yaht_game *game) : buffered_socket(), game_(game)
+{
+}
+
+yaht_client_factory::yaht_client_factory(yaht_game *game) : game_(game)
+{
+}
+
+
+void yaht_client::on_connect()
+{
+    json::object packet;
+
+    packet.set_int("action", CLIENT_INIT);
+    packet.set_string("name", (*yaht::engine::instance())[0]->name());
+
+    write(packet);
+}
+
+void yaht_client::on_close()
+{
+    // game_->action_remove_network_player( );
+}
+
+void yaht_client::on_will_read()
+{
+    //log::trace(format("{0} will read", sock->getIP()));
+}
+
+void yaht_client::on_did_read()
+{
+    json::object packet = json::object(readln());
+
+    client_action action = (client_action) packet.get_int("action");
+
+    switch (action)
+    {
+    case CLIENT_INIT:
+    {
+        string name = packet.get_string("name");
+
+        game_->action_add_network_player(name);
+
+        break;
+    }
+    }
+}
+
+void yaht_client::on_will_write()
+{
+    //log::trace(format("{0} will write", sock->getIP()));
+}
+
+void yaht_client::on_did_write()
+{
+}
+
+
+std::shared_ptr<buffered_socket> yaht_client_factory::create_socket(socket_server *server, SOCKET sock, const sockaddr_in &addr)
+{
+    auto socket = make_shared<yaht_client>(game_, sock, addr);
+
+    return socket;
+}
---
+++ src/yaht_client.h
@@ -0,0 +1,44 @@
+#ifndef _YAHT_CLIENT_H_
+#define _YAHT_CLIENT_H_
+
+#include <arg3net/socket_factory.h>
+#include <arg3net/buffered_socket.h>
+#include <map>
+
+class yaht_game;
+
+typedef enum
+{
+    CLIENT_INIT
+} client_action;
+
+class yaht_client : public arg3::net::buffered_socket
+{
+public:
+    yaht_client(yaht_game *game, arg3::net::SOCKET sock, const sockaddr_in &addr);
+    yaht_client(yaht_game *game);
+
+    virtual void on_will_read();
+    virtual void on_did_read();
+    virtual void on_will_write();
+    virtual void on_did_write();
+    virtual void on_connect();
+    virtual void on_close();
+
+private:
+    yaht_game *game_;
+};
+
+class yaht_client_factory : public arg3::net::socket_factory
+{
+public:
+    yaht_client_factory(yaht_game *game);
+
+    std::shared_ptr<arg3::net::buffered_socket> create_socket(arg3::net::socket_server *server, arg3::net::SOCKET sock, const sockaddr_in &addr);
+
+private:
+    yaht_game *game_;
+    std::map<arg3::net::buffered_socket *, string> connections_;
+};
+
+#endif
